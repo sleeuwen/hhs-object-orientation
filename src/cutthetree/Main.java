d05b8c97c7e4b228f97ba8440314d197483558b9
michaelvs97 <michael_V_S@hotmail.com>
2017-03-17 09:45:51 +0100

    Frame aangemaakt
    Frame aangemaakt ook in main

--- src/cutthetree/Main.java
+++ src/cutthetree/Main.java
@@ -1,8 +1,16 @@
 package cutthetree;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        JFrame window = new PlayFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("CutThaTree");
+        window.setResizable(false);
+        window.setVisible(true);
     }
 }
---
+++ src/cutthetree/PlayFrame.java
@@ -0,0 +1,19 @@
+package cutthetree;
+
+import javax.swing.*;
+
+/**
+ * Created by Michael on 17-3-2017.
+ */
+public class PlayFrame extends JFrame {
+
+    private static int FRAME_HEIGHT = 800;
+    private static int FRAME_WIDTH = 800;
+
+    public PlayFrame(){
+        setSize(FRAME_HEIGHT, FRAME_WIDTH);
+    }
+
+
+
+}
