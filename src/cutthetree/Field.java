38d2bfbb5a6238d80d0d140316778ea61bdd0552
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2017-03-17 10:14:43 +0100

    Flipped FRAME_HEIGHT and FRAME_WIDTH parameters to `JFrame#setSize()`

--- src/cutthetree/Field.java
+++ src/cutthetree/Field.java
@@ -6,7 +6,7 @@ import java.awt.*;
  * Created by The lion kings on 17-3-2017.
  */
 public class Field {
-    protected int xPos,yPos;
+    protected int xPos, yPos;
     protected boolean isSolid = false;
 
     public void paint(Graphics g) {
--- src/cutthetree/PlayFrame.java
+++ src/cutthetree/PlayFrame.java
@@ -6,11 +6,10 @@ import javax.swing.*;
  * Created by The lion kings on 17-3-2017.
  */
 public class PlayFrame extends JFrame {
-
-    private static int FRAME_HEIGHT = 800;
-    private static int FRAME_WIDTH = 800;
+    private final static int FRAME_HEIGHT = 800;
+    private final static int FRAME_WIDTH = 800;
 
     public PlayFrame(){
-        setSize(FRAME_HEIGHT, FRAME_WIDTH);
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
     }
 }
