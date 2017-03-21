3fbfad90bee989ef893fd1cbea9ea0449ce3aafe
michaelvs97 <michael_V_S@hotmail.com>
2017-03-21 13:03:22 +0100

    lumberaxetest

---
+++ test/cutthetree/LumberaxeTest.java
@@ -0,0 +1,31 @@
+package cutthetree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Michael on 21-3-2017.
+ */
+public class LumberaxeTest {
+
+    Lumberaxe lumberaxe1;
+    @Before
+    public void setUp() throws Exception {
+        lumberaxe1 = new Lumberaxe(0,0,Color.RED);
+
+    }
+
+    @Test
+    public void getColorPositive() throws Exception {
+        assertTrue(lumberaxe1.getColor() == Color.RED);
+    }
+
+    @Test
+    public void getColorNegative() throws Exception {
+        assertFalse(lumberaxe1.getColor() == Color.BLACK);
+    }
+
+
+}
