7b251aa8a977c5115173efa367d05a679b43caa4
koenvzeijl <kvanzeijl@hotmail.com>
2017-03-21 13:12:02 +0100

    playerTest

--- test/cutthetree/PlayerTest.java
+++ test/cutthetree/PlayerTest.java
@@ -11,39 +11,49 @@ import static org.junit.Assert.*;
 public class PlayerTest {
 
     Player player1;
+    Lumberaxe axe;
     @Before
     public void setUp() throws Exception {
         player1 = new Player(1,1);
+        axe = new Lumberaxe(2,5,Color.BLACK);
     }
 
     @Test
     public void movePositive() throws Exception {
-
+        player1.move(1,1);
+        assertTrue(player1.xPos == 2 && player1.yPos ==2);
     }
 
     @Test
-    public void changeDirection() throws Exception {
-
+    public void moveNegative() throws Exception {
+        player1.move(1,1);
+        assertFalse(player1.xPos == 5 && player1.yPos ==7);
     }
 
     @Test
-    public void getDirection() throws Exception {
-
+    public void changeDirectionPositive() throws Exception {
+        player1.changeDirection(Direction.LEFT);
+        assertEquals(Direction.LEFT, player1.getDirection());
     }
 
     @Test
-    public void grabLumberaxe() throws Exception {
-
+    public void changeDirectionNegative() throws Exception {
+        player1.changeDirection(Direction.LEFT);
+        assertNotEquals(Direction.UP, player1.getDirection());
     }
 
     @Test
-    public void getAxe() throws Exception {
-
+    public void grabLumberaxePositive() throws Exception {
+        player1.grabLumberaxe(axe);
+        assertEquals(axe,player1.getAxe());
     }
 
     @Test
-    public void say() throws Exception {
-
+    public void grabLumberaxeNegative() throws Exception {
+        player1.grabLumberaxe(axe);
+        Lumberaxe wrong = new Lumberaxe(5,5,Color.WHITE);
+        assertNotEquals(wrong,player1.getAxe());
     }
+
 
 }
