037b73fcda131b2bb2d458c502267f571002a5b8
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2017-03-24 15:17:42 +0100

    Ensure inner size is 900x900

--- src/cutthetree/GameFrame.java
+++ src/cutthetree/GameFrame.java
@@ -1,21 +1,22 @@
 package cutthetree;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Instances of this class will create a new {@link JFrame} with
  * the correct size.
  */
 public class GameFrame extends JFrame {
-    protected final static int FRAME_HEIGHT = 929;
+    protected final static int FRAME_HEIGHT = 900;
     protected final static int FRAME_WIDTH = 900;
 
     public GameFrame() {
-        getContentPane().setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        getContentPane().setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
         setTitle("Cut The Tree");
         setResizable(false);
 
         add(new Game());
+        pack();
     }
 }
